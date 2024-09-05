96796ddc0454e0fa73b6fb1a0ad7c882a67bdd1d
Andres U <urizandres@estudiantec.cr>
2024-09-04 21:50:31 -0600

    Pagina inicial

---
+++ src/components/HeroSection.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "../App.css";
+import "./HeroSection.css";
+
+function HeroSection() {
+  return (
+    <div className="hero-container">
+      <h1>LOGRA TUS METAS</h1>
+      <p>Con NutriTEC</p>
+    </div>
+  );
+}
+
+export default HeroSection;
